d985411307c4d9439624c5fe683682bff4ddd968
Enock Chikobvu <92862163+enock-chi@users.noreply.github.com>
2024-05-14 10:23:07 +0200

    slefcare page progress

--- section/SelfCare.jsx
+++ section/SelfCare.jsx
@@ -1,26 +1,55 @@
-import React from 'react'
-import Image from 'next/image'
-import selfcare from '@/temp/selfcare'
-import Link from 'next/link'
-import banner from '@/public/images/IMG_70m.jpg'
+"use client";
+
+import React, { useState } from "react";
+import Image from "next/image";
+import selfcare from "@/temp/selfcare";
+import Link from "next/link";
+import banner from "@/public/images/IMG_70.jpg";
 
 const SelfCare = () => {
+  const [active, setActive] = useState(0);
+  const [images, setImages] = useState(selfcare[active]);
+
   return (
-    <div className='w-full h-auto flex flex-col justify-center'>
-        <hr className='w-full border-black'></hr>
-        <div className='flex w-full h-auto'>
-          <div className='group w-[20vw] h-[85vh] border-r border-black flex flex-col items-center pt-[8vh] space-y-[12vh] overflow-hidden'>
-            {selfcare.map((item,i) => (
-              <Link key={i} className='text-black w-[30vw] text-center text-[1.5rem] font-extrabold rotate-[-90deg] hover:border border-black rounded-[1.1rem] shadow-xl ' href={'#'}>{item.name}</Link>
+    <div className="w-full h-auto flex flex-col justify-center">
+      <hr className="w-full border-black"></hr>
+      <div className="flex w-full h-auto">
+        <div className="group w-[20vw] h-[85vh] border-r border-black flex flex-col items-center pt-[8vh] space-y-[12vh] overflow-hidden">
+          {selfcare.map((item, i) => (
+            <button
+              key={i}
+              className="border-none text-black w-[30vw] text-center text-[1.5rem] font-extrabold rotate-[-90deg] group-hover:border rounded-[1.1rem] shadow-xl"
+            >
+              {item.name}
+            </button>
+          ))}
+        </div>
+        <div className="w-[80vw] h-[85vh] relative flex items-center justify-center">
+          <Image
+            src={banner}
+            alt="https://unsplash.com/@fabulu75"
+            style={{ objectFit: "cover" }}
+            className="h-full"
+          />
+          <div className="grid grid-cols-2 gap-2 absolute top-0 w-[80vw] h-full p-4 items-center justify-center">
+            {images.icons.map((item, i) => (
+              <div
+                key={i}
+                className="backdrop-blur-lg flex items-center justify-center rounded-2xl"
+              >
+                <Image
+                  src={`/images/${item.name}`}
+                  alt=""
+                  width={200}
+                  height={200}
+                />
+              </div>
             ))}
           </div>
-          <div className='w-[80vw] h-[85vh] relative'>
-              <Image src={banner} alt='https://unsplash.com/@fabulu75' style={{objectFit: "cover"}} className='h-full'/>
-          </div>
         </div>
-
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default SelfCare
+export default SelfCare;
--- temp/selfcare.js
+++ temp/selfcare.js
@@ -1,8 +1,46 @@
 const selfcare = [
-  { name: 'Salon', link: '/' },
-  { name: 'Nailbar', link: '/' },
-  { name: 'Hairbar', link: '/about' },
-  { name: 'Men', link: '/services' }
+  {
+    name: "Salon",
+    icons: [
+      { name: "skin.png", link: "" },
+      { name: "nails.png", link: "" },
+      { name: "waxing.png", link: "" },
+      { name: "brows-lashes.png", link: "" },
+      { name: "massages.png", link: "" },
+      { name: "mens-grooming.png", link: "" },
+    ],
+  },
+  {
+    name: "Nailbar",
+    icons: [
+      { name: "manis-pedis.png", link: "" },
+      { name: "gel.png", link: "" },
+      { name: "acrylic.png", link: "" },
+      { name: "nail-art.png", link: "" },
+      { name: "brows-lashes", link: "" },
+    ],
+  },
+  {
+    name: "Hairbar",
+    icons: [
+      { name: "blowouts-styling.png", link: "" },
+      { name: "treatments.png", link: "" },
+      { name: "nails.png", link: "" },
+      { name: "extensions-braids.png", link: "" },
+      { name: "brows-lashes.png", link: "" },
+    ],
+  },
+  {
+    name: "Men",
+    icons: [
+      { name: "freshcuts-colour.png", link: "" },
+      { name: "beard-care.png", link: "" },
+      { name: "shaves.png", link: "" },
+      { name: "skin.png", link: "" },
+      { name: "waxing.png", link: "" },
+      { name: "hands-feet.png", link: "" },
+    ],
+  },
 ];
 
 export default selfcare;
