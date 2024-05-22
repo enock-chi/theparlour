c3956eb5bae6be7ef724ce35d297c1d1d91799de
Enock Chikobvu <92862163+enock-chi@users.noreply.github.com>
2024-05-22 22:07:37 +0200

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -28,8 +28,8 @@ const Footer = () => {
           </Link>
         </div>
         <p className="text-black mx-auto w-[60vw] h-auto py-[1vh] text-center font-bold text-[1.2rem]">
-          Be the first to hear about new treatments, promotions and tips to get
-          that feeling.
+          <span className="text-pink-500">Be the first </span>to hear about new
+          treatments, promotions and tips to get that feeling.
         </p>
         <div className="pb-[3vh]">
           <FooterDropDown title="Services" options="" />
@@ -37,9 +37,9 @@ const Footer = () => {
           <FooterDropDown title="Company" options="" />
         </div>
         <hr className="w-full border-black" />
-        <div className="isolate bg-white pt-2">
+        <div className="isolate bg-white pt-5">
           <div
-            className="absolute h-[66rem] inset-x-0 top-[-13rem] -z-10 transform-gpu overflow-hidden blur-3xl "
+            className="absolute h-[61rem] inline-block inset-x-0 top-[-13rem] -z-10 transform-gpu overflow-hidden blur-3xl "
             aria-hidden="true"
           >
             <div
@@ -65,7 +65,7 @@ const Footer = () => {
             />
           </div>
           <div className="w-full text-center">
-            <h2 className="text-3xl font-bold tracking-tight text-gray-900 ">
+            <h2 className="text-3xl font-bold tracking-tight text-black ">
               Contact Us
             </h2>
           </div>
@@ -74,79 +74,79 @@ const Footer = () => {
               <div className="flex items-center space-x-10">
                 <label
                   htmlFor="first-name"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
+                  className="block text-center text-xl w-[35vw] font-bold leading-6 text-gray-900"
                 >
                   First name
                 </label>
-                <div className="mt-2.5">
+                <div className="mt-2.5 w-full">
                   <input
                     type="text"
                     name="first-name"
                     id="first-name"
                     autoComplete="given-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block h-[3vh] w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-black placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="flex items-center space-x-10">
                 <label
                   htmlFor="last-name"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
+                  className="block text-center text-xl w-[35vw] font-semibold leading-6 text-gray-900"
                 >
                   Last name
                 </label>
-                <div className="mt-2.5">
+                <div className="mt-2.5 w-full">
                   <input
                     type="text"
                     name="last-name"
                     id="last-name"
                     autoComplete="family-name"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full h-[3vh] rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-black placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="flex items-center space-x-10">
                 <label
                   htmlFor="company"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
+                  className="block text-center text-xl w-[35vw] font-semibold leading-6 text-gray-900"
                 >
                   Company
                 </label>
-                <div className="mt-2.5">
+                <div className="mt-2.5 w-full">
                   <input
                     type="text"
                     name="company"
                     id="company"
                     autoComplete="organization"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full h-[3vh] rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-black placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="flex items-center space-x-10">
                 <label
                   htmlFor="email"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
+                  className="block text-center text-xl w-[35vw] font-semibold leading-6 text-gray-900"
                 >
                   Email
                 </label>
-                <div className="mt-2.5">
+                <div className="mt-2.5 w-full">
                   <input
                     type="email"
                     name="email"
                     id="email"
                     autoComplete="email"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full h-[3vh] rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-black placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="flex items-center space-x-10">
                 <label
                   htmlFor="phone-number"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
+                  className="block text-center text-xl w-[35vw] font-semibold leading-6 text-gray-900"
                 >
                   Phone number
                 </label>
-                <div className="relative mt-2.5">
+                <div className="relative mt-2.5 w-full">
                   <div className="absolute inset-y-0 left-0 flex items-center">
                     <label htmlFor="country" className="sr-only">
                       Country
@@ -154,7 +154,7 @@ const Footer = () => {
                     <select
                       id="country"
                       name="country"
-                      className="h-full rounded-md border-0 bg-transparent bg-none py-0 pl-4 text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
+                      className="h-[3vh] rounded-md border-0 bg-transparent bg-none py-0 pl-4 text-black focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
                     >
                       <option>+27</option>
                     </select>
@@ -164,14 +164,14 @@ const Footer = () => {
                     name="phone-number"
                     id="phone-number"
                     autoComplete="tel"
-                    className="block w-full rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full h-[3vh] rounded-md border-0 px-3.5 py-2 pl-20 text-gray-900 shadow-sm ring-1 ring-inset ring-black placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
               <div className="sm:col-span-2">
                 <label
                   htmlFor="message"
-                  className="block text-sm font-semibold leading-6 text-gray-900"
+                  className="block text-center mt-[1vh] text-xl font-semibold leading-6 text-gray-900"
                 >
                   Message
                 </label>
@@ -180,7 +180,7 @@ const Footer = () => {
                     name="message"
                     id="message"
                     rows={4}
-                    className="block w-full rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full h-[10vh] rounded-md border-0 px-3.5 py-2 text-gray-900 shadow-sm ring-1 ring-inset ring-black placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     defaultValue=""
                   />
                 </div>
@@ -191,8 +191,8 @@ const Footer = () => {
                     checked={enabled}
                     onChange={setEnabled}
                     className={classNames(
-                      enabled ? "bg-indigo-600" : "bg-gray-200",
-                      "flex w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      enabled ? "bg-pink-500" : "bg-gray-200",
+                      "flex mt-[4.1vh] w-8 flex-none cursor-pointer rounded-full p-px ring-1 ring-inset ring-gray-900/5 transition-colors duration-200 ease-in-out focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                     )}
                   >
                     <span className="sr-only">Agree to policies</span>
@@ -205,9 +205,9 @@ const Footer = () => {
                     />
                   </Switch>
                 </div>
-                <Switch.Label className="text-sm leading-6 text-gray-600">
+                <Switch.Label className="text-sm font-bold leading-6 text-gray-600 mt-[2vh]">
                   By selecting this, you agree to our{" "}
-                  <a href="#" className="font-semibold text-indigo-600">
+                  <a href="#" className="font-semibold text-pink-500">
                     privacy&nbsp;policy
                   </a>
                   .
@@ -217,7 +217,7 @@ const Footer = () => {
             <div className="mt-6">
               <button
                 type="submit"
-                className="block w-full rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                className="block w-full rounded-md bg-pink-500 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 Let&apos;s talk
               </button>
